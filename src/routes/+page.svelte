b561ef60649799dd2efd58deae42bdcec4c40db2
leugiim <miguelrl.92@gmail.com>
2024-02-04 20:47:32 +0100

    build: config linter and tailwind (#1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-2xl text-primary">Text 1</h1>
+<h2 class="text-2xl text-secondary">Text 2</h2>
+<h2 class="text-2xl text-muted">Text 3</h2>
+<h2 class="text-2xl text-light">Text 4</h2>
+<h2 class="text-2xl text-alternative">Text 5</h2>
+<h2 class="text-2xl text-dark">Text 6</h2>
