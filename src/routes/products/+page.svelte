c8d61864c4535408274e5c0df1ef96303d315314
leugiim <miguelrl.92@gmail.com>
2024-02-06 01:16:41 +0100

    create table

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Table from '$lib/components/Table.svelte';
+	import { getInitialFetchResult } from '$lib/fetch';
+	import { useProducts } from '$lib/fetch/fetchProducts';
+	import type { Product } from '$lib/types/Product';
+	import { onMount } from 'svelte';
+
+	const { status, httpStatus, message, content: products } = getInitialFetchResult<Product[]>();
+
+	onMount(async () => {
+		useProducts({ status, httpStatus, message, content: products });
+	});
+
+	const headers = ['id', 'name', 'price'];
+</script>
+
+<Table {headers} data={$products} />
