94cbcb4744201dafb0fce9d3366b571102e1028b
leugiim <miguelrl.92@gmail.com>
2024-02-06 01:15:06 +0100

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<nav class="h-[100px] bg-primary text-muted flex items-center justify-end">
+	<a href="/" class="mr-auto">Home</a>
+	<a href="/products">Products</a>
+	<a href="/discounts">Discounts</a>
+	<a href="/cart">Cart</a>
+	<a href="/about">About</a>
+</nav>
+
+<style>
+	a {
+		@apply p-6 h-full bg-primary hover:brightness-125 uppercase flex justify-center items-center;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 </script>
+
+<Header />
 
 <main class="container h-screen mx-auto flex items-center justify-center">
 	<slot />
